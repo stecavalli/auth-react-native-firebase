dbdb895532ae01764166e099141944d05bf468b2
Stefano Cavalli <stefanocavalli@outlook.com>
2025-05-25 12:45:47 +0200

    Update RootNavigator.tsx

--- components/navigators/RootNavigator.tsx
+++ components/navigators/RootNavigator.tsx
@@ -5,15 +5,40 @@ import ProfileScreen from "../../screens/ProfileScreen";
 import RegisterScreen from "../../screens/RegisterScreen";
 import EditProfileScreen from "../../screens/EditProfileScreen";
 
-const Stack = createNativeStackNavigator();
+// 1. Definiamo il tipo delle rotte
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Profile: undefined;
+  EditProfile: undefined;
+};
+
+// 2. Creiamo lo Stack Navigator con il tipo
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-	  <Stack.Screen name="Register" component={RegisterScreen} />
-	  <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+      <Stack.Screen 
+	    name="Login" 
+	    component={LoginScreen}
+        options={{ headerShown: false }}	  
+	  />
+      <Stack.Screen 
+	    name="Profile" 
+		component={ProfileScreen}
+        options={{ headerShown: false }}		 
+	  />
+	  <Stack.Screen  
+	    name="Register" 
+		component={RegisterScreen} 
+		options={{ headerShown: false }} 
+	  />
+	  <Stack.Screen 
+	    name="EditProfile" 
+	    component={EditProfileScreen} 
+	    options={{ title: "Pagina del profilo" }}
+	  />
     </Stack.Navigator>
   );
 }
